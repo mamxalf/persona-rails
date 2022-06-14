9533d9d56e3895038d360f861bd4296e69354a76
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-06-14 07:46:59 +0700

    rspec validate auth

--- app/domains/core/repositories/user/create_spec.rb
+++ app/domains/core/repositories/user/create_spec.rb
@@ -12,15 +12,21 @@ RSpec.describe Core::Repositories::User::Create do
       role:         'user'
     }
   end
+
   describe '#RESULT' do
     context 'when call ping' do
       let(:result) do
         described_class.new(params).call
       end
-      it 'successfully response monad' do
+      it 'should be successfully' do
         expect(result.success?).to be true
         expect(result.success.class).to eql Core::Entities::User
         expect(result.success.attributes).to eql params.except(:password, :role)
+
+        # validate auth
+        auth = Auth.find_by_email(params[:email])
+        expect(auth.valid_password?(params[:password])).to eql true
+        expect(auth.auth_able).to eql User.find_by_email(params[:email])
       end
     end
   end
