7a0deb24e7f0b8cd609ac63f2943885d7d43a8b9
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-06-15 21:32:37 +0700

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_235123) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_131452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_235123) do
     t.index ["auth_able_type"], name: "index_auths_on_auth_able_type"
     t.index ["email"], name: "index_auths_on_email", unique: true
     t.index ["reset_password_token"], name: "index_auths_on_reset_password_token", unique: true
+  end
+
+  create_table "examples", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
