81862f830249d18f62f1fa4171671ec5b2bd316c
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-06-15 21:32:11 +0700

    update password entity optional

--- app/domains/core/entities/member.rb
+++ app/domains/core/entities/member.rb
@@ -1,6 +1,6 @@
 class Core::Entities::Member < Core::Entities::AbstractEntity
   attribute :email, Types::Strict::String
-  attribute :password, Types::Strict::String
+  attribute :password, Types::Strict::String.optional
   attribute :role, Types::String.enum('user', 'admin', 'super_admin', 'trial')
   attribute :subscription, Types::Params::Date
 end
