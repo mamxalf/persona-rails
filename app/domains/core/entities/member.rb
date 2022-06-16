7bb08c1b1d47942cdc95b46e009312c1a6a5bd9a
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-06-16 22:56:28 +0700

    adjust entity

--- app/domains/core/entities/member.rb
+++ app/domains/core/entities/member.rb
@@ -1,6 +1,8 @@
 class Core::Entities::Member < Core::Entities::AbstractEntity
+  attribute :id, Types::Strict::String
   attribute :email, Types::Strict::String
   attribute :password, Types::Strict::String.optional
   attribute :role, Types::String.enum('user', 'admin', 'super_admin', 'trial')
+  attribute :is_active, Types::Strict::Bool
   attribute :subscription, Types::Params::Date
 end
