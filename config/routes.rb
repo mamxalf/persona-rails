085703cea7851d1337afc08c10cd55da77f8aa45
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-06-16 22:55:11 +0700

    members routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :members, :only => [:index, :new, :create, :show]
   resources :examples
-  resources :member, :only => [:index, :new, :create]
   get 'user/new'
   get 'user/create'
   devise_for :auths
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'dashboard#index'
 end
